dc8ec6778dce65fae10392fecace8906f7421d6b
Benn314 <209441806@qq.com>
2023-08-23 19:52:03 +0800

    docs:vue基础语法完结

--- src/08_综合案例/index.js
+++ src/08_综合案例/index.js
@@ -40,11 +40,15 @@ Vue.createApp({
       for (let book of this.books) {
         finalPrice += book.count * book.price
       }
+      // 比较笨的方法是不做过滤，直接在return处加单位
       return finalPrice
     },
     // Vue3不支持过滤器了，推荐两种做法：使用计算属性和使用全局的方法
+    // 这种方式用得还是蛮多的，有时服务器返回的数据，格式并不是我们想要的，可以通过filter的map、item方式来做过滤转化
+    // 但是采用字符串拼接和filter都不是最佳选择，我们可以封装成一个method进行调用
     filterBooks() {
       return this.books.map(item => {
+        // 这里只做第一层拷贝
         const newItem = Object.assign({}, item); // 之所以多做一份拷贝是为了原数组对象books不被破坏，不然totalPrice显示为NaN，因为乘法无法用于字符串
         newItem.price = "￥" + item.price;
         return newItem;
@@ -61,6 +65,11 @@ Vue.createApp({
     },
     removeBook(index) {
       this.books.splice(index, 1)
+    },
+    // 这里我们用方法实现过滤器的效果
+    // 我们当前formatPrice只对当前组件有效，如果我们希望其他组件也能用到，可以放到全局 app.config.globalProperties（后面会讲）相当于实现全局filter效果
+    formatPrice(price) {
+      return '￥' + price
     }
 
   },
